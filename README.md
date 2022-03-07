3e5956432b9c24cc44fff8b4cab11c131058c272
Godlia <52934844+Godlia@users.noreply.github.com>
2022-03-07 14:59:41 +0100

    l

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 # CRUD-Oppgave
 ## Kravspesifikasjon
+
+Lage en nettside som følger CRUD og skal vise frem dataen og muligheter for å utføre CRUD operasjoner.
+
+### CRUD OPERASJONER
 * Må kunne create tables (create)
 * Må kunne reade fra tables (select)
 * Må kunne update tables (update)
--- index.php
+++ index.php
@@ -1,14 +1,78 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>CRUD</title>
 </head>
+
 <body>
-    <?php 
-    echo "gaming";
+    <?php
+    $servername = 'localhost';
+    $username = 'root';
+    $password = '';
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password);
+
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    } else {
+        echo "Connected successfully" . "<br>";
+    }
+
+
+    ?>
+
+    <form action="" method="get">
+        <label for="name">create table with the name: </label><input type="text" name="name">
+        <input type="hidden" value="create">
+        <input type="submit" value="Submit">
+    </form>
+    <form action="" method="get">
+        <label for="name">delete table with the name: </label><input type="text" name="name">
+        <input type="hidden" value="delete">
+        <input type="submit" value="Submit">
+    </form>
+    <form action="" method="get">
+        <label for="name">insert into table with the name: </label><input type="text" name="name">
+        <input type="hidden" value="insert">
+        <input type="submit" value="Submit">
+    </form>
+    <form action="" method="get">
+        <label for="name">update table with the name: </label><input type="text" name="name">
+        <input type="hidden" value="update">
+        <input type="submit" value="Submit">
+    </form>
+
+    <?php
+    $query = "SELECT * FROM crud";
+    $result = $conn->query($query);
+    echo $query . $result;
+    if ($result->num_rows > 0) {
+        // output data of each row
+        while ($row = $result->fetch_assoc()) {
+            echo "id: " . $row["id"] . "name: " . $row["name"] .  "<br>";
+        }
+    } else {
+        echo "0 results";
+    }
+
+    ?>
+
+
+
+
+    <?php
+    if (isset($_GET['name'])) {
+        $sql = $_GET['name'];
+        $result = $conn->query($sql);
+        echo $result;
+    }
     ?>
 </body>
+
 </html>
